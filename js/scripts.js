62f372e527c85c6f8183d3a2baf6b6996846723c
Mariana-WA <92222496+Mariana-WA@users.noreply.github.com>
2021-11-07 11:10:32 +0100

    added burger menu

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+
+  console.log("Pura Vida! Welcome to Rainforest Dairy");
+
+
+  function myFunction() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+  }
